a83270310cff87aec5c448d6dd9954540979cbfa
dgascon <dgascon@stud.42lyon.fr>
2022-02-24 21:29:52 +0100

    edit button, add class

---
+++ src/menus/buttons/quit.py
@@ -0,0 +1,17 @@
+from src.menus.buttons import Button
+import pygame
+
+class QuitButton(Button):
+    def __init__(
+        self, 
+        surface: pygame.Surface, 
+        rect: tuple,
+        func: any,
+        text: str = "Quit", 
+        background_color: tuple = (0, 0, 0, 150), 
+        text_color: tuple = (255, 0, 0),
+        hovering_color: tuple = (255, 255, 255),
+        text_size: int = 75):
+        super().__init__(surface, rect, func, text, background_color, text_color, hovering_color, text_size)
+
+    
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,5 +3,4 @@ from .draws import draw_rect_alpha
 from .colors import reduce_alpha_on_color, increase_alpha_on_color
 
 from .dialog import DialogBox
-from .button import Button
 from .get_font import get_font
